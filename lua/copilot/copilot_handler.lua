d82190186a9021fc50f1a29dc75a32dadeeb3c19
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-04-04 20:09:49 -0400

    fix

--- lua/copilot/copilot_handler.lua
+++ lua/copilot/copilot_handler.lua
@@ -1,5 +1,6 @@
 local user_data = require('copilot.setup').get_cred()
 local util = require('copilot.util')
+local M = {}
 
 local send_editor_info = function (a, b, c, d)
    local responses = vim.lsp.buf_request_sync(0, 'setEditorInfo', {
@@ -17,33 +18,37 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.getCompletions = true
 
-vim.lsp.start_client({
-   cmd = {require('copilot.util').get_copilot_path()},
-   cmd_env = {
-      ["GITHUB_USER"] = user_data.user,
-      ["GITHUB_TOKEN"] = user_data.token,
-      ["COPILOT_AGENT_VERBOSE"] = 1,
-   },
-   handlers={
-      ["getCompletions"] = function () print("get completions") end,
-      ["textDocumentSync"] = function () print("handle") end,
-   },
-   name = "copilot",
-   trace = "messages",
-   root_dir = vim.loop.cwd(),
-   autostart = true,
-   on_init = function(client, _)
-      vim.lsp.buf_attach_client(0, client.id)
-      vim.api.nvim_create_autocmd({'BufEnter'}, {
-         callback = function ()
-            if not vim.lsp.buf_get_clients(0)[client.id] then
-               vim.lsp.buf_attach_client(0, client.id)
-            end
-         end,
-         once = false,
-      })
-   end,
-   on_attach = function()
-      send_editor_info()
-   end
-})
+M.start = function (plugin_path)
+   vim.lsp.start_client({
+      cmd = {require('copilot.util').get_copilot_path(plugin_path)},
+      cmd_env = {
+         ["GITHUB_USER"] = user_data.user,
+         ["GITHUB_TOKEN"] = user_data.token,
+         ["COPILOT_AGENT_VERBOSE"] = 1,
+      },
+      handlers={
+         ["getCompletions"] = function () print("get completions") end,
+         ["textDocumentSync"] = function () print("handle") end,
+      },
+      name = "copilot",
+      trace = "messages",
+      root_dir = vim.loop.cwd(),
+      autostart = true,
+      on_init = function(client, _)
+         vim.lsp.buf_attach_client(0, client.id)
+         vim.api.nvim_create_autocmd({'BufEnter'}, {
+            callback = function ()
+               if not vim.lsp.buf_get_clients(0)[client.id] then
+                  vim.lsp.buf_attach_client(0, client.id)
+               end
+            end,
+            once = false,
+         })
+      end,
+      on_attach = function()
+         send_editor_info()
+      end
+   })
+end
+
+return M
