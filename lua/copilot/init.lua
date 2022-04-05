34c32358293cfaa25e60cbb0d9e89dea3debd8f7
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2022-04-05 20:11:52 +0200

    setup function and package manager independence fix

--- lua/copilot/init.lua
+++ lua/copilot/init.lua
@@ -1,1 +1,11 @@
-require("copilot.copilot_handler").start()
+local config = require("copilot.config")
+local M = {}
+
+M.setup = function (params)
+    config.setup(params)
+    config.params.plugin_manager_path = vim.fn.expand(config.params.plugin_manager_path) -- resolve wildcard and variable containing paths
+    require("copilot.copilot_handler").start(config.params.plugin_manager_path)
+end
+
+
+return M
