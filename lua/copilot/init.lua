9a83f1000fa69cb3f7e7c6a5b3417ae86e067e5d
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-12 04:23:18 -0400

    Update README.md with new features

--- lua/copilot/init.lua
+++ lua/copilot/init.lua
@@ -1,28 +1,16 @@
 local M = { client_info = nil }
 local client = require("copilot.client")
-local lsp = vim.lsp
 
 local defaults = {
+  cmp_method = "getCompletionsCycling",
+  ft_disable = {},
   plugin_manager_path = vim.fn.stdpath("data") .. "/site/pack/packer",
   server_opts_overrides = {},
-  ft_disable = {},
-  cmp_method = "getCompletionsCycling"
 }
 
 local config_handler = function(opts)
   local user_config = opts and vim.tbl_deep_extend("force", defaults, opts) or defaults
   return user_config
-end
-
-local set_client_info = function(client_id)
-  M.client_info = {
-    client_id = client_id,
-    client = lsp.get_client_by_id(client_id)
-  }
-end
-
-local get_client_info = function ()
-  return M.client_info
 end
 
 M.setup = function(opts)
