ba3bc443e93d95d566255da1deb6d5f776c4d16c
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-12 04:05:33 -0400

    Added CopilotPanel command to preview options in window

--- lua/copilot/panel.lua
+++ lua/copilot/panel.lua
@@ -1,10 +1,11 @@
 local util = require("copilot.util")
 local format = require("copilot_cmp.format")
+local handler = require("copilot.handlers")
+local print_buf = require("copilot.print_panel")
 
 local panel = {
   method = "getPanelCompletions",
   usecmp = false,
-  cache_line = true,
   buf = "",
   uri = "",
 }
@@ -18,36 +19,34 @@ panel.send_request = function (callback)
   vim.lsp.buf_request(0, panel.method, completion_params, callback)
 end
 
-panel.complete = vim.schedule_wrap(function (_, params, callback)
-  local context = params.context
+local verify_existing = function (context)
   existing_matches[context.bufnr] = existing_matches[context.bufnr] or {}
   existing_matches[context.bufnr][context.cursor.row] = existing_matches[context.bufnr][context.cursor.row] or {}
+end
+
+panel.complete = vim.schedule_wrap(function (_, params, callback)
+  local context = params.context
+  verify_existing(context)
 
   local add_completion = function (result)
-    if result then
-      result.text = result.displayText
-      local formatted = format.format_item(params, result)
-      existing_matches[context.bufnr][context.cursor.row][formatted.label] = formatted
-      vim.schedule(function() callback({
-        isIncomplete = true,
-        items = vim.tbl_values(existing_matches[context.bufnr][context.cursor.row])
-      }) end)
-    end
+    result.text = result.displayText
+    local formatted = format.format_item(params, result)
+    existing_matches[context.bufnr][context.cursor.row][formatted.label] = formatted
+    callback({
+      isIncomplete = true,
+      items = vim.tbl_values(existing_matches[context.bufnr][context.cursor.row])
+    })
   end
 
   local completed = function ()
-    vim.schedule(function() callback({
+    callback({
       isIncomplete = false,
       items = vim.tbl_values(existing_matches[context.bufnr][context.cursor.row])
-    }) end)
-    if not panel.cache_line then
-      existing_matches[context.bufnr][context.cursor.row] = {}
-    end
+    })
   end
 
-  local handler = require("copilot.handlers").add_handler_callback
-  handler("PanelSolution", "cmp", add_completion)
-  handler("PanelSolutionsDone", "cmp", completed)
+  handler.add_handler_callback("PanelSolution", "cmp", add_completion)
+  handler.add_handler_callback("PanelSolutionsDone", "cmp", completed)
 
   panel.send_request()
 
@@ -59,19 +58,38 @@ function panel.create (opts)
   panel.buf = type(panel.uri) == "number" or vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_name(panel.buf, "copilot:///" .. tostring(panel.buf))
   panel.uri = vim.uri_from_bufnr(panel.buf)
+
   vim.api.nvim_create_user_command("CopilotPanel", function ()
-    local panel_suggestions = {}
-    local handlers = require("copilot.handlers")
-    handlers.add_handler_callback("PanelSolution", "print", function (result)
-      table.insert(panel_suggestions, format.clean_insertion(result.displayText))
+    print_buf.create(panel.buf)
+    local items = {}
+    handler.add_handler_callback("PanelSolution", "pb", function (result)
+      local formatted = format.deindent(result.displayText)
+      items[formatted] = 1
     end)
-    handlers.add_handler_callback("PanelSolutionsDone", "print", function ()
-      local print_buf = require("copilot.print_buf").init()
-      print_buf.print(panel_suggestions)
-      handlers.remove_handler_callback("PanelSolution", "print")
-      handlers.remove_handler_callback("PanelSolutionsDone", "print")
+    handler.add_handler_callback("PanelSolutionsDone", "pb", function ()
+      local item_list = vim.tbl_add_reverse_lookup(vim.tbl_keys(items))
+      local result_text = vim.tbl_flatten(vim.tbl_map(function(v)
+        local s = vim.fn.split(v, '\n')
+        local text = vim.tbl_map(function (t)
+          local number_string = "[" .. item_list[v] .. "]"
+          local str = (s[1] == t and number_string .. string.rep(' ', vim.o.shiftwidth)) or string.rep(' ', vim.o.shiftwidth+string.len(number_string))
+          return str .. t
+        end, s)
+        table.insert(text, '')
+        return text
+      end, item_list))
+      print_buf.set_text(result_text)
+      items = {}
     end)
-    panel.send_request()
+    vim.api.nvim_create_autocmd("WinClosed", {
+      pattern = { tostring(print_buf.win) },
+      callback = function ()
+        print("CopilotPanel closed")
+        handler.remove_handler_callback("PanelSolution", "pb")
+        handler.remove_handler_callback("PanelSolutionsDone", "pb")
+      end,
+      once = true,
+    })
   end, {})
   return panel
 end
