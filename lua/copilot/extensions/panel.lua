3417fde78b49baf7ea14bebe4e1d0269f7732827
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-13 18:48:57 -0400

    fix and improve cmp panel completions

--- lua/copilot/extensions/panel.lua
+++ lua/copilot/extensions/panel.lua
@@ -6,15 +6,19 @@ local print_buf = require("copilot.extensions.print_panel")
 local panel = {
   method = "getPanelCompletions",
   usecmp = false,
+  client = vim.lsp.get_active_clients({name="copilot"})[1],
   buf = "",
   uri = "copilot:///placeholder",
+  requests = {},
 }
 
 panel.send_request = function (callback)
+  panel.client = not vim.tbl_isempty(panel.client) and panel.client or vim.lsp.get_active_clients({name="copilot"})[1]
+  if not panel.client then return end
   local completion_params = util.get_completion_params()
   completion_params.panelId = panel.uri
   callback = callback or function () end
-  vim.lsp.buf_request(0, panel.method, completion_params, callback)
+  return panel.client.rpc.request(panel.method, completion_params, callback)
 end
 
 local existing_matches= {}
@@ -27,15 +31,12 @@ end
 panel.complete = vim.schedule_wrap(function (_, params, callback)
   local context = params.context
   verify_existing(context)
+  local sent, id
 
   local add_completion = function (result)
     result.text = result.displayText
     local formatted = format.format_item(params, result)
     existing_matches[context.bufnr][context.cursor.row][formatted.label] = formatted
-    callback({
-      isIncomplete = true,
-      items = vim.tbl_values(existing_matches[context.bufnr][context.cursor.row])
-    })
   end
 
   local completed = function ()
@@ -47,9 +48,8 @@ panel.complete = vim.schedule_wrap(function (_, params, callback)
 
   handler.add_handler_callback("PanelSolution", "cmp", add_completion)
   handler.add_handler_callback("PanelSolutionsDone", "cmp", completed)
-
-  panel.send_request()
-
+  if sent and id then panel.client.rpc.cancel_request(id) end
+  sent, id = panel.send_request()
   callback({ isIncomplete = true })
 end)
 
@@ -58,6 +58,7 @@ function panel.create (max_results)
   panel.buf = type(panel.uri) == "number" or vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_name(panel.buf, "copilot:///" .. tostring(panel.buf))
   panel.uri = vim.uri_from_bufnr(panel.buf)
+  panel.client = vim.lsp.get_active_clients({name="copilot"})
 
   vim.api.nvim_create_user_command("CopilotPanel", function ()
     panel.send_request()
