7d5eb7ecb9e6bc8f46c1ab51fc6c24d30e718fd8
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-15 16:52:54 -0400

    remove redundant method

--- lua/copilot/handlers.lua
+++ lua/copilot/handlers.lua
@@ -7,12 +7,11 @@ local lsp_handlers = {
 
 local handlers = {
   ["PanelSolution"] = function (_, result, _, config)
-    if result then
-      for _, callback in pairs(config.callbacks) do
-        callback(result)
-      end
-    end
+    if not result then return "err" end
+    if result.panelId then config.callbacks[result.panelId](result)
+    else for _, callback in pairs(config.callbacks) do callback() end end
   end,
+
   ["PanelSolutionsDone"] = function (_, _, _, config)
     for _, callback in pairs(config.callbacks) do
       callback()
@@ -20,7 +19,6 @@ local handlers = {
   end
 }
 
--- require name so not confusing
 lsp_handlers.add_handler_callback = function (handler, fn_name, fn)
   lsp_handlers.callbacks[handler][fn_name] = fn
   vim.lsp.handlers[handler] = vim.lsp.with(handlers[handler], {
@@ -33,6 +31,12 @@ lsp_handlers.remove_handler_callback = function (handler, fn_name)
   vim.lsp.handlers[handler] = vim.lsp.with(handlers[handler], {
     callbacks = lsp_handlers.callbacks[handler]
   })
+end
+
+lsp_handlers.remove_all_name = function (fn_name)
+  for handler, _ in pairs(lsp_handlers.callbacks) do
+    lsp_handlers.remove_handler_callback(handler, fn_name)
+  end
 end
 
 return lsp_handlers
