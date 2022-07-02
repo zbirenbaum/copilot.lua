22a040a47708d46b1cae5f2f0efaad3d66e804c6
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-07-02 19:03:16 -0400

    add CopilotDetach and CopilotStop commands

--- lua/copilot/init.lua
+++ lua/copilot/init.lua
@@ -15,6 +15,19 @@ local defaults = {
 }
 
 local create_cmds = function (_)
+  vim.api.nvim_create_user_command("CopilotDetach", function()
+    local client_instance = require("copilot.util").get_copilot_client()
+    local valid = client_instance and vim.lsp.buf_is_attached(0, client_instance.id)
+    if not valid then return end
+    vim.lsp.buf_detach_client(0, client_instance.id)
+  end, {})
+
+  vim.api.nvim_create_user_command("CopilotStop", function()
+    local client_instance = require("copilot.util").get_copilot_client()
+    if not client_instance then return end
+    vim.lsp.stop_client(client_instance.id)
+  end, {})
+
   vim.api.nvim_create_user_command("CopilotPanel", function ()
     local panel = require("copilot.extensions.panel").create()
     panel.send_request()
