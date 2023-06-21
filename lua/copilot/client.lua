zbirenbaum/copilot.lua

commit 26bb7ec51fcd9c30d0c888f09eb61870f3fc9b51

get_node_version() not working with custom vim.opt.shell

lua/copilot/client.lua
@@ -49,7 +49,7 @@ function M.get_node_version()
   if not M.node_version then
     local node = config.get("copilot_node_command")
 
-    local cmd = node .. " --version"
+    local cmd = { "node", "--version" }
     local cmd_output_table = vim.fn.systemlist(cmd, nil, false)
     local cmd_output = cmd_output_table[#cmd_output_table]
     local cmd_exit_code = vim.v.shell_error

commit 718d26042a682c24af68bb815b1b76790b0fe5ac

Update lua/copilot/client.lua
Fixed bug in get_node_version() introduced in 26bb7ec

Co-authored-by: Munif Tanjim <hello@muniftanjim.dev>

lua/copilot/client.lua
@@ -49,7 +49,7 @@ function M.get_node_version()
   if not M.node_version then
     local node = config.get("copilot_node_command")
 
-    local cmd = { "node", "--version" }
+    local cmd = { node, "--version" }
     local cmd_output_table = vim.fn.systemlist(cmd, nil, false)
     local cmd_output = cmd_output_table[#cmd_output_table]
     local cmd_exit_code = vim.v.shell_error