7e0960ccffd7bd02fd83bb8eb16d86faa9d22d43
Munif Tanjim <hello@muniftanjim.dev>
2023-11-29 02:02:51 +0600

    feat: bump to copilot.vim 1.12.0

--- lua/copilot/util.lua
+++ lua/copilot/util.lua
@@ -20,7 +20,7 @@ function M.get_editor_info()
     editorPluginInfo = {
       name = "copilot.lua",
       -- reflects version of github/copilot.vim
-      version = "1.11.4",
+      version = "1.12.0",
     },
   }
   return info
