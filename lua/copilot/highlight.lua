e7f89f8769af2069af7ca3ff668e9f3cb8442758
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-03-23 19:17:44 -0400

    fix: highlight themes not being honored
    Fixes #376

--- lua/copilot/highlight.lua
+++ lua/copilot/highlight.lua
@@ -11,9 +11,16 @@ local links = {
 }
 
 function mod.setup()
-  for from_group, to_group in pairs(links) do
-    vim.api.nvim_command("highlight default link " .. from_group .. " " .. to_group)
-  end
+  -- Some environments will load themes after plugins (like ChadNv) so we do it as late as possible
+  vim.schedule(function()
+    vim.print("Setting up copilot highlight groups")
+    for from_group, to_group in pairs(links) do
+      local ok, existing = pcall(vim.api.nvim_get_hl, 0, { name = from_group })
+      if not ok or vim.tbl_isempty(existing) then
+        vim.api.nvim_set_hl(0, from_group, { link = to_group })
+      end
+    end
+  end)
 end
 
 return mod
