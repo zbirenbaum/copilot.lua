91bf40a791b208184799882bb128920e20b0adee
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-04-07 19:56:13 -0400

    test: update tests for auth and debugger

--- tests/test_base_to_organize.lua
+++ tests/test_base_to_organize.lua
@@ -1,6 +1,7 @@
 local eq = MiniTest.expect.equality
 local child = MiniTest.new_child_neovim()
--- local env = require("tests.env")
+local env = require("tests.env")
+local utils_debug = require("tests.utils_debug")
 
 local T = MiniTest.new_set({
   hooks = {
@@ -9,13 +10,18 @@ local T = MiniTest.new_set({
       child.lua([[M = require('copilot')]])
       child.lua([[c = require('copilot.command')]])
       child.lua([[s = require('copilot.status')]])
-      -- child.fn.setenv("GITHUB_COPILOT_TOKEN", env.COPILOT_TOKEN)
+      child.fn.setenv("GITHUB_COPILOT_TOKEN", env.COPILOT_TOKEN)
+      utils_debug.launch_lua_debugee(child)
     end,
     post_once = child.stop,
   },
 })
 
+-- TODO: find a way for autocmd or something
 local function run_setup()
+  -- utils_debug.attach_to_debugee()
+  vim.loop.sleep(10000)
+  -- vim.wait(0)
   child.lua([[M.setup({
     logger = {
       file_log_level = vim.log.levels.TRACE,
--- tests/test_client.lua
+++ tests/test_client.lua
@@ -1,6 +1,6 @@
 local child = MiniTest.new_child_neovim()
 local u = require("tests.utils")
--- local env = require("tests.env")
+local env = require("tests.env")
 
 local T = MiniTest.new_set({
   hooks = {
@@ -18,7 +18,7 @@ local T = MiniTest.new_set({
       child.lua([[a = require('copilot.api')]])
       child.lua("logger = require('copilot.logger')")
       -- child.lua([[require("osv").launch({ port = 8086 })]])
-      -- child.fn.setenv("GITHUB_COPILOT_TOKEN", env.COPILOT_TOKEN)
+      child.fn.setenv("GITHUB_COPILOT_TOKEN", env.COPILOT_TOKEN)
     end,
     post_once = child.stop,
   },
