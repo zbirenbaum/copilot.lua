2378a40e9cecbf3720b6f4f3d5d6b6d8994bd321
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-04-09 21:32:27 -0400

    chore: bump Copilot LSP version to 1.300

--- lua/copilot/util.lua
+++ lua/copilot/util.lua
@@ -13,7 +13,7 @@ function M.get_editor_info()
     editorPluginInfo = {
       name = "copilot.lua",
       -- reflects version of github/copilot-language-server-release
-      version = "1.298.0",
+      version = "1.300.0",
     },
   }
   return info
