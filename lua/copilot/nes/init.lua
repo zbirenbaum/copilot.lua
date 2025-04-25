9681e1d8edee473955bd0972d1bf9430dd1cb7da
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-04-25 15:40:44 -0400

    WIP on nes

--- lua/copilot/nes/init.lua
+++ lua/copilot/nes/init.lua
@@ -2,14 +2,22 @@ local nes_ui = require("copilot.nes.ui")
 local utils = require("copilot.nes.util")
 local api = require("copilot.api")
 local c = require("copilot.client")
+local client_utils = require("copilot.client.utils")
+local logger = require("copilot.logger")
 
 local M = {}
 
 local nes_ns = vim.api.nvim_create_namespace("copilot-nes")
+local req_id = nil
 
 ---@param err lsp.ResponseError?
 ---@param result copilotlsp.copilotInlineEditResponse
 local function handle_nes_response(err, result)
+  logger.trace("request handled: " .. req_id, result)
+  -- req_id = nil
+  -- utils.with_client(function(client)
+  --   api.notify_shown(client, { uuid = uuid }, function() end)
+  -- end)
   if err then
     -- vim.notify(err.message)
     return
@@ -18,7 +26,9 @@ local function handle_nes_response(err, result)
     --- Convert to textEdit fields
     edit.newText = edit.text
   end
-  nes_ui._display_next_suggestion(result.edits, nes_ns)
+  vim.schedule(function()
+    nes_ui._display_next_suggestion(result.edits, nes_ns)
+  end)
 end
 
 --- Requests the NextEditSuggestion from the current cursor position
@@ -27,8 +37,21 @@ function M.request_nes()
   local version = vim.lsp.util.buf_versions[vim.api.nvim_get_current_buf()]
   ---@diagnostic disable-next-line: inject-field
   pos_params.textDocument.version = version
+
+  if req_id then
+    c.use_client(function(client)
+      client_utils.wrap(client):cancel_request(req_id)
+    end)
+  end
+
   c.use_client(function(client)
-    api.request(client, "textDocument/copilotInlineEdit", pos_params, handle_nes_response)
+    local _, id, ctx = api.request(client, "textDocument/copilotInlineEdit", pos_params, handle_nes_response)
+    logger.trace("new request: " .. id, ctx)
+    req_id = id
+  end)
+
+  c.use_client(function(client)
+    client_utils.wrap(client):cancel_request(req_id)
   end)
 end
 
