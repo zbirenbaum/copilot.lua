a8871658094c62538e58d4b7c2b6954cb98abcf7
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-07-28 11:04:41 -0400

    WIP

--- tests/test_client.lua
+++ tests/test_client.lua
@@ -57,17 +57,17 @@ T["client()"]["suggestions work when multiple files open with should_attach logi
   child.config.should_attach = [[function(bufnr, bufname)
     buffername = bufname:match("([^/\\]+)$")
 
-    if not _G.buffername then
-      _G.buffername = ''
+    if not _G.bufnames then
+      _G.bufnames  = ''
     end
 
-    _G.buffername = _G.buffername .. ' ; ' .. buffername.. ' (' .. tostring(bufnr) .. ')'
+    _G.bufnames = _G.bufnames .. ' ; ' .. buffername.. ' (' .. tostring(bufnr) .. ')'
    
-    if (bufnr == 2) then
+    -- if (buffername == "file1.txt") or (buffername == "file2.txt") then
       return true
-    else
-      return false, "incorrect buffer" 
-    end
+    -- else
+      -- return false, "incorrect buffer" 
+    -- end
   end]]
   child.config.suggestion = child.config.suggestion .. "auto_trigger = true,"
 
@@ -78,7 +78,7 @@ T["client()"]["suggestions work when multiple files open with should_attach logi
   -- child.cmd("e tests/files/file3.txt")
   child.type_keys("123", "<Esc>", "o456", "<Esc>", "o7")
   child.wait_for_suggestion()
-  -- local buffername = child.lua("return tostring(_G.buffername)")
+  local bufnames = child.lua("return tostring(_G.bufnames)")
 
   -- local messages = child.lua([[
   -- return vim.api.nvim_exec("messages", { output = true }) or ""
@@ -88,7 +88,7 @@ T["client()"]["suggestions work when multiple files open with should_attach logi
   -- buffername = buffername:match("([^/\\]+)$")
 
   -- u.expect_match(buffername, "file3.txt")
-  -- print("Buffername: " .. buffername)
+  print("bufnames: " .. bufnames)
   -- print("Messages: " .. messages)
 
   reference_screenshot(child.get_screenshot(), nil, { ignore_text = { 9, 10 }, ignore_attr = { 9, 10 } })
