8cfbb5a9b1a130d726b5fa4ae64c6a68db311983
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2025-08-27 21:09:35 -0400

    feat: add support for `Copilot panel toggle` command
    fixes 241

--- tests/test_command.lua
+++ tests/test_command.lua
@@ -1,6 +1,7 @@
 local child_helper = require("tests.child_helper")
 local child = child_helper.new_child_neovim("test_suggestion")
 local u = require("tests.utils")
+local reference_screenshot = MiniTest.expect.reference_screenshot
 
 local T = MiniTest.new_set({
   hooks = {
@@ -20,6 +21,19 @@ T["command()"]["version works"] = function()
   child.cmd("Copilot version")
   local result = child.cmd_capture("mess")
   u.expect_match(result, ".*copilot language server.*copilot%.lua.*Node%.js.*language%-server%.js.*")
+end
+
+T["command()"]["panel toggle - open works"] = function()
+  child.configure_copilot()
+  child.cmd("Copilot panel toggle")
+  reference_screenshot(child.get_screenshot())
+end
+
+T["command()"]["panel toggle - close works"] = function()
+  child.configure_copilot()
+  child.cmd("Copilot panel toggle")
+  child.cmd("Copilot panel toggle")
+  reference_screenshot(child.get_screenshot())
 end
 
 return T
