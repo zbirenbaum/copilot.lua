665a724faef59c837d638f890b76019b0b4f97c2
Alex Efros <powerman@powerman.name>
2026-03-04 12:02:30 +0200

    fix: race in should_attach

--- lua/copilot/config/should_attach.lua
+++ lua/copilot/config/should_attach.lua
@@ -4,14 +4,14 @@ local logger = require("copilot.logger")
 
 local should_attach = {
   ---@type ShouldAttachFunc
-  default = function(_, _)
-    if not vim.bo.buflisted then
+  default = function(buf_id, _)
+    if not vim.bo[buf_id].buflisted then
       logger.debug("not attaching, buffer is not 'buflisted'")
       return false
     end
 
-    if vim.bo.buftype ~= "" then
-      logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+    if vim.bo[buf_id].buftype ~= "" then
+      logger.debug("not attaching, buffer 'buftype' is " .. vim.bo[buf_id].buftype)
       return false
     end
 
